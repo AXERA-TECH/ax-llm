f28d5ae545b36e600fdafa63cf95a808c218149d
ZHEQIUSHUI <qiushui.zhe@gmail.com>
2025-03-31 17:41:20 +0800

    支持加载和保存 system prompt

--- src/runner/LLM.hpp
+++ src/runner/LLM.hpp
@@ -234,7 +234,14 @@ public:
         b_stop = true;
     }
 
-    int GenerateKVCache(std::string system_prompt)
+    int SetSystemPrompt(std::string system_prompt, std::vector<int> &_token_ids)
+    {
+        tokenizer->Reset(system_prompt, _token_ids);
+        _attr.system_prompt = system_prompt;
+        return 0;
+    }
+
+    int GenerateKVCache(std::vector<int> &_token_ids)
     {
         // clear kv cache
         for (size_t i = 0; i < _attr.axmodel_num; i++)
@@ -242,10 +249,6 @@ public:
             memset((void *)llama_layers[i].layer.get_input(decode_grpid, "K_cache").pVirAddr, 0, llama_layers[i].layer.get_input(decode_grpid, "K_cache").nSize);
             memset((void *)llama_layers[i].layer.get_input(decode_grpid, "V_cache").pVirAddr, 0, llama_layers[i].layer.get_input(decode_grpid, "V_cache").nSize);
         }
-
-        std::vector<int> _token_ids;
-        tokenizer->Reset(system_prompt, _token_ids);
-        _attr.system_prompt = system_prompt;
 
         bfloat16 bf16 = -65536.f;
         std::vector<unsigned short> mask(_attr.kv_cache_num + 1, bf16.data);
