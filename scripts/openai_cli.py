6a4ba2d7c3796893f07cd083323eb8eeb7d054cf
ZHEQIUSHUI <qiushui.zhe@gmail.com>
2025-11-11 14:07:37 +0800

    gradio demo 支持视频理解

--- scripts/openai_cli.py
+++ scripts/openai_cli.py
@@ -1,7 +1,9 @@
 import base64
+import glob
 from openai import OpenAI
 import cv2
 
+BASE_URL = "http://localhost:8000/v1"
 
 def img_to_data_url(img_path: str):
     img = cv2.imread(img_path)
@@ -14,30 +16,51 @@ def img_to_data_url(img_path: str):
     return f"data:image/jpeg;base64,{b64}"
 
 
-image_data = img_to_data_url("/home/axera/ax-llm/demo_cv308/frame_0075.jpg")
-# print(image_data)
+def test(openai_messages):
+    client = OpenAI(api_key="not-needed", base_url=BASE_URL)
 
-BASE_URL = "http://localhost:8000/v1"
-client = OpenAI(api_key="not-needed", base_url=BASE_URL)
+    stream = client.chat.completions.create(
+        model="AXERA-TECH/Qwen3-VL-2B-Instruct-GPTQ-Int4",
+        messages=openai_messages,
+        stream=True,
+    )
+    out_chunks = []
+    for ev in stream:
+        delta = ev.choices[0].delta
+        if delta and delta.content:
+            out_chunks.append(delta.content)
+            print(delta.content, end="", flush=True)
+    print()
+    assistant_text = "".join(out_chunks).strip()
 
-openai_messages = {
-    "role": "user",
-    "content": [
-        {"type": "text", "text": "描述一下这张图片"},
-        {"type": "image_url", "image_url": image_data},
-    ],
-}
+def test_image():
+    image_data = img_to_data_url("../demo_cv308/frame_0075.jpg")
 
-stream = client.chat.completions.create(
-    model="AXERA-TECH/Qwen3-VL-2B-Instruct-GPTQ-Int4",
-    messages=openai_messages,
-    stream=True,
-)
-out_chunks = []
-for ev in stream:
-    delta = ev.choices[0].delta
-    if delta and delta.content:
-        out_chunks.append(delta.content)
-        print(delta.content, end="", flush=True)
-print()
-assistant_text = "".join(out_chunks).strip()
+    openai_messages = {
+        "role": "user",
+        "content": [
+            {"type": "text", "text": "描述一下这张图片"},
+            {"type": "image_url", "image_url": image_data},
+        ],
+    }
+    
+
+    test(openai_messages)
+
+def test_video():
+    image_list = glob.glob("../demo_cv308/*.jpg")
+    image_list.sort()
+    
+    image_data_list = [img_to_data_url(img) for img in image_list]
+    
+    openai_messages = {
+        "role": "user",
+        "content": [
+            {"type": "text", "text": "描述一下这个视频"},
+            {"type": "image_url", "is_video":True, "image_url": image_data_list},
+        ],
+    }
+    
+    test(openai_messages)
+
+test_video()
