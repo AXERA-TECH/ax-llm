01a97a21331ccd5fc18b369b06c0d50df930e365
lihongjie <lihongjie@axera-tech.com>
2025-12-30 12:14:09 +0800

    update

---
+++ scripts/run_image.sh
@@ -0,0 +1,20 @@
+AXMODEL_DIR=./Qwen3-VL-2B-Instruct-AX650-c128_p1152-int4/
+
+./main \
+--template_filename_axmodel "${AXMODEL_DIR}/qwen3_vl_text_p128_l%d_together.axmodel" \
+--axmodel_num 28 \
+--filename_image_encoder_axmodedl "${AXMODEL_DIR}/Qwen3-VL-2B-Instruct_vision.axmodel" \
+--use_mmap_load_embed 1 \
+--filename_tokenizer_model "qwen3_tokenizer.txt" \
+--filename_post_axmodel "${AXMODEL_DIR}/qwen3_vl_text_post.axmodel" \
+--filename_tokens_embed "${AXMODEL_DIR}/model.embed_tokens.weight.bfloat16.bin" \
+--tokens_embed_num 151936 \
+--tokens_embed_size 2048 \
+--patch_size 16 \
+--live_print 1 \
+--video 0 \
+--img_width 384 \
+--img_height 384 \
+--vision_start_token_id 151652 \
+--post_config_path post_config.json \
+--devices 0,
