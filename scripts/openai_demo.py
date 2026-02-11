c89cdab2e6fc1eceaa51609ab6d9a1bf2db13e5d
ZHEQIUSHUI <qiushui.zhe@gmail.com>
2026-02-11 15:01:43 +0800

    新增支持 Smollm2

--- scripts/openai_demo.py
+++ scripts/openai_demo.py
@@ -4,36 +4,44 @@ from openai import OpenAI
 import requests
 import json
 import re
+import argparse
 
-# Base URL of your API server; adjust host and port as needed
-API_URL = "http://10.126.33.252:8000/v1"
-MODEL = "AXERA-TECH/Qwen3-1.7B"
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description='OpenAI Demo')
+    parser.add_argument('--model', type=str, default=MODEL, help='Model name')
+    parser.add_argument('--api_url', type=str, default=API_URL, help='API URL', default="http://10.126.33.252:8000/v1")
+    args = parser.parse_args()
 
-messages = [
-    {
-        "role": "system",
-        "content": [{"type": "text", "text": "your name is Lisa,you are a helpful assistant."}]
-    },
-    {
-        "role": "user",
-        "content": "你好"
-    }
-]
+    # Base URL of your API server; adjust host and port as needed
+    API_URL = args.api_url
+    MODEL = args.model
 
-# 调后端（流式）
-client = OpenAI(api_key="not-needed", base_url=API_URL.strip())
-stream = client.chat.completions.create(
-    model=MODEL.strip(),
-    messages=messages,
-    stream=True,
-)
+    messages = [
+        {
+            "role": "system",
+            "content": [{"type": "text", "text": "you are a helpful assistant."}]
+        },
+        {
+            "role": "user",
+            "content": "hello"
+        }
+    ]
 
-print("assistant:", end="\n")
-# 逐 chunk 更新 assistant 气泡（Markdown）
-for ev in stream:
-    delta = getattr(ev.choices[0], "delta", None)
-    if delta and getattr(delta, "content", None):
-        ctx = delta.content
-        print(ctx, end="", flush=True)
-print("\n")
+    # 调后端（流式）
+    client = OpenAI(api_key="not-needed", base_url=API_URL.strip())
+    stream = client.chat.completions.create(
+        model=MODEL.strip(),
+        messages=messages,
+        stream=True,
+    )
+
+    print("assistant:", end="\n")
+    # 逐 chunk 更新 assistant 气泡（Markdown）
+    for ev in stream:
+        delta = getattr(ev.choices[0], "delta", None)
+        if delta and getattr(delta, "content", None):
+            ctx = delta.content
+            print(ctx, end="", flush=True)
+    print("\n")
 
